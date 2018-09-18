be615fec001f3749cf1eea0bcf3530cb5e4ea70f
Priyanka Rai <Priyanka.Rai@capgemini.com>
2018-09-18 11:43:33 +0530

    Changes

---
+++ Practice/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+ public class Main
+ {
+public static void main(String args[])
+ 
+ {
+        String alphabet= "abcdefghijklmnopqrstuvwxyz";
+        String s = "TestData";
+        Random random = new Random();
+        int randomLen = 1+random.nextInt(9);
+        for (int i = 0; i < randomLen; i++) 
+        {
+            char c = alphabet.charAt(random.nextInt(26));
+            int integer = random.nextInt(12);
+            s+=c;
+            s= s+integer;
+        
+        }
+		System.out.println(s);
+        
+    }
+}
